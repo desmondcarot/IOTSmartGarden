c882ab38aebf65307aab851fb71f47509ee59518
kendr <kendrickfooloktim@gmail.com>
2024-05-05 21:28:31 +0800

    Bird module update

--- app/src/main/java/com/edu/tarc/iotproject/BirdFeed.kt
+++ app/src/main/java/com/edu/tarc/iotproject/BirdFeed.kt
@@ -5,32 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.edu.tarc.iotproject.databinding.ActivityAquariumBinding
-import com.edu.tarc.iotproject.databinding.ActivityBirdFeedBinding
 
 class BirdFeed : AppCompatActivity() {
-
-    private lateinit var binding: ActivityBirdFeedBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityBirdFeedBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_bird_feed)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        binding.birdTitle.setText("Nice")
-
-        binding.birdTitle.setOnClickListener{
-            binding.birdTitle.setText("three")
-        }
-
-
-
-
-
     }
 }
